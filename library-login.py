b68363a0c326d26ba41f3f136e07d3f75e162671
Mystery3051 <alexandr.job@yahoo.com>
2020-02-23 01:57:44 -0500

    Fixed typo.

--- library-login.py
+++ library-login.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     options.add_argument('headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    options.binary_location = os.environ['GOOGLE_CHROME_PATH']
+    options.binary_location = os.environ['GOOGLE_CHROME_BIN']
     # if chrome_binary:
     #     options.binary_location = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
     driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER_PATH'],
